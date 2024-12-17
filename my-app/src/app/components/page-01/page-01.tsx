76a281d0dd5e31f48a9f2cf3deaae5603c380216
khan561hunter <waniakhan561@gmail.com>
2024-12-17 23:20:22 +0500

    changes again

--- my-app/src/app/components/page-01/page-01.tsx
+++ my-app/src/app/components/page-01/page-01.tsx
@@ -1,23 +1,24 @@
 "use client";
 import Image from "next/image"
-import find from "@/app/assets/FIND CLOTHES THAT MATCHES YOUR STYLE.png"
 
 import rec from "@/app/assets/Rectangle 2 (1).png"
+import find from "@/app/assets/FIND CLOTHES THAT MATCHES YOUR STYLE.png"
 import vector from "@/app/assets/Vector.png"
+import boygirl from "@/app/assets/Rectangle 2.png"
 import vector2 from "@/app/assets/Vector (1).png"
 import num from "@/app/assets/Frame 56.png"
 import num1 from "@/app/assets/Frame 57.png"
 import rec2 from "@/app/assets/Rectangle 2 (2).png"
-import boygirl from "@/app/assets/Rectangle 2.png"
+import Responsive1 from "./responsive";
 
 
 
 export default function One(){
     return(
-        <div className=" flex items-center relative">
-            <Image className=" hidden lg:block w-[1440px]  h-[663px]   absolute top-[15px]" src={rec} alt="rect"></Image>
-            <Image className=" absolute  top-[431px]  left-[750px]" src={vector} alt="vector"></Image>
-            <Image className=" absolute  top-[160px]  left-[1200px]" src={vector2} alt="vector"></Image>
+        <div className=" flex items-center relative  ">
+            <Image className=" hidden md:block w-[1440px]  h-[663px]   absolute top-[15px]" src={rec} alt="rect"></Image>
+            <Image className=" hidden md:block absolute  top-[431px]  left-[750px]" src={vector} alt="vector"></Image>
+            <Image className=" hidden md:block absolute  top-[160px]  left-[1200px]" src={vector2} alt="vector"></Image>
             
             
             
@@ -47,60 +48,11 @@ export default function One(){
                 </div>
 
             </div>
+            <Responsive1 />
 
 
-            {/* for mobile screen */}
-            <div className="lg:hidden ">
+          
             
-                <div className=" h-[853px] bg-[#F2F0F1] w-screen flex flex-col  py-8">
-                    <Image className="w-[319px] h-[93px]  h-auto px-4  ml-1 relative" src={find} alt="find"></Image>
-                    <div className=" w-[358px]   h-[50px] mx-4 p-4 text-left  ">
-                        <p className="font-normal  text-[14px]  leading-[20px] text-[#00000099]  ">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-                    </div>
-                    <div className="w-[358px] h-[52px]  bg-[black] rounded-[62px] m-4 my-[45px]  ">
-                        <p className="text-center  text-white font-medium text-[16px] leading-[21.6px] text-nowrap px-[65px] py-[16px] ">Shop Now</p>
-                    </div>
-                    <div className=" w-[278px] h-[52px]  flex items-center justify-between m-[75px] mt-[-35px] grid grid-cols-2 ">
-                        <div className="w-[106px] h-[48px]  ">
-                            <p className="w-[64px] h-[32px] text-[24px] leading-[54px] text-black font-bold  ">200+</p>
-                            <p className="w-[106px] h-[22px] py-2 font-normal text-[12px] leading-[22px] text-[#00000099] text-nowrap font-satoshi">International Brands</p>
-                        </div>
-                    
-                        <div className="w-[106px] h-[48px] ">
-                            <p className="ww-[64px] h-[32px] text-[24px] font-bold leading-[54px] text-black text-[24px] ">2,000+</p>
-                            <p className="w-[106px] h-[22px] py-2 font-normal text-[12px] leading-[22px] text-[#00000099] text-nowrap font-satoshi">Happy Customers</p>
-                        </div>
-                        <div className="w-[106px] h-[48px] mx-[55px] p-2 ">
-                            <p className="w-[64px] h-[32px] text-[24px] font-bold leading-[54px] text-black text-[24px] ">30,000+</p>
-                            <p className="w-[106px] py-2  h-[22px] font-normal text-[12px] leading-[22px] text-[#00000099] text-nowrap font-satoshi">High-Quality Products</p>
-                        </div>
-                        
-                        
-
-                    </div>
-                    
-                    
-                    <div>
-                        
-                    <Image src={boygirl} alt="background"></Image>
-                    <div className="z-20 relative my-[-70%] mx-[10%]">
-
-                    <Image className="w-[44px] h-[44px]" src={vector} alt="vector"></Image>
-                    <Image className=" w-[76px] h-[76px] mx-[80%] my-[-60%]" src={vector2} alt="vector"></Image>
-
-                    </div>
-
-
-                    
-                    </div>
-                    
-                    
-                    
-                </div>
-                
-                
-
-            </div>
 
 
 
